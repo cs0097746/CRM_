ed796524945f4e4081e5520c06adb178cc66ebf7
Christian Schneider <65926491+cs0097746@users.noreply.github.com>
2025-09-15 16:28:40 -0300

    feat: layout kanban

--- backend/atributo/views.py
+++ backend/atributo/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import AtributoPersonalizavel
+from .serializers import AtributoPersonalizavelSerializer
 
-# Create your views here.
+class AtributoPersonalizavelListView(generics.ListAPIView):
+    """
+    Esta view retorna uma lista de todos os atributos personalizáveis disponíveis.
+    """
+    queryset = AtributoPersonalizavel.objects.all()
+    serializer_class = AtributoPersonalizavelSerializer
+    # Se você tiver permissões, adicione aqui. Ex: permission_classes = [IsAuthenticated]
