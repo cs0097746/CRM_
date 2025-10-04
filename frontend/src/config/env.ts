c8436a6678d0b2da1cfc39e90a8e7f42ad941970
Diogo <dapereira@inf.ufsm.br>
2025-10-04 15:03:33 -0300

    feat: ajuste api url

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -2,6 +2,6 @@ const isDevelopment = import.meta.env.DEV;
 
 const backend_url = isDevelopment 
   ? 'http://localhost:8000'  // ✅ Desenvolvimento
-  : 'https://backend.loomiecrm.com';   // ✅ Produção (altere quando fizer deploy)
+  : 'https://backend.loomiecrm.com/';   // ✅ Produção (altere quando fizer deploy)
 
 export default backend_url;
