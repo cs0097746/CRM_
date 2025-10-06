5c8fd31d5e1732fc3b28c67c30d4115952243a2d
Diogo <dapereira@inf.ufsm.br>
2025-10-06 18:27:35 -0300

    hotfix: /o/token

--- frontend/src/views/Atendimento.tsx
+++ frontend/src/views/Atendimento.tsx
@@ -271,7 +271,7 @@ export default function Atendimento() {
       params.append("client_secret", CLIENT_SECRET);
 
       try {
-        const res = await axios.post(`${backend_url}/o/token/`, params);
+        const res = await axios.post(`${backend_url}o/token/`, params);
         return res.data.access_token;
       } catch (err) {
         console.error(err);
