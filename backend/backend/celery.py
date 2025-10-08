8e70254fa54aea042083487ca386a367040ebeb3
Diogo <dapereira@inf.ufsm.br>
2025-10-08 15:22:10 -0300

    feat: celery + redis p tarefas

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -2,6 +2,12 @@ import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-app = Celery('backend')
+
+from django.conf import settings
+
+app = Celery('backend',
+             broker=settings.CELERY_BROKER_URL,
+             backend=settings.CELERY_RESULT_BACKEND)
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -397,8 +397,8 @@ if DEBUG:
         'localhost',
     ]
 
-CELERY_BROKER_URL = 'redis://redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/2'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/2'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
