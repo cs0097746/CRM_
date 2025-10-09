4761d0ec0e19d4d4bce0c999378faa2b026dffcd
Diogo <dapereira@inf.ufsm.br>
2025-10-09 08:55:01 -0300

    feat: teste do celery

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -397,8 +397,9 @@ if DEBUG:
         'localhost',
     ]
 
-CELERY_BROKER_URL = 'redis://redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_BROKER_URL = 'redis://redis_crm:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis_crm:6379/0'
+
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
